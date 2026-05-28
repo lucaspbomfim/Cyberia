c202b617d1d912ca74be66c7c7d7843177226daf
lucaspbomfim <foks1903@gmail.com>
2026-05-28 12:59:31 -0300

    resolve conflito de merge no user service

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-=======
-from fastapi import FastAPI
->>>>>>> 25b38e64379d0424e6ba91fd0501def1932bf081
 from database import init_db
 from routes import router
 
 app = FastAPI()
 
-<<<<<<< HEAD
 # Permite que o frontend (porta 3000) acesse esta API
 app.add_middleware(
     CORSMiddleware,
@@ -23,8 +16,3 @@ app.add_middleware(
 
 init_db()
 app.include_router(router)
-=======
-init_db()
-
-app.include_router(router)
->>>>>>> 25b38e64379d0424e6ba91fd0501def1932bf081
