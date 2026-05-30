834d585ce1836d61ab3566182a0c2f9f0085dce5
Arthur Fragamota <aarthurfragamota@gmail.com>
2026-05-30 13:22:19 -0300

    add render deploy config to user-service

--- services/user-service/main.py
+++ services/user-service/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import init_db
@@ -5,14 +6,13 @@ from routes import router
 
 app = FastAPI()
 
-# Permite que o frontend (porta 3000) acesse esta API
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # em produção, troque pela URL da Vercel
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 init_db()
+
 app.include_router(router)
